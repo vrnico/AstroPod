70f618ffc543023816ba2856c5d7747d084e0f04
vrnico <nico.daunt@gmail.com>
2018-05-11 18:27:12 -0700

    figured out how to use Astro NuGet and set debugger lines accordingly

--- AstroPod/Controllers/HomeController.cs
+++ AstroPod/Controllers/HomeController.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AstroPod.Models;
+using AstroAlgo;
+using AstroAlgo.Base;
+using AstroAlgo.SolarSystem;
+using AstroAlgo.Model;
 
 namespace AstroPod.Controllers
 {
@@ -12,9 +16,30 @@ namespace AstroPod.Controllers
     {
         public IActionResult Index()
         {
+            Equator equator = Sun.EquatorialCoordinate(DateTime.Now);
+            Ecliptic mooquator = Moon.EclipticalCoordinate(DateTime.Now);
+            Ecliptic merquator = Mercury.EclipticalCoordinate(DateTime.Now);
+            Ecliptic vequator = Venus.EclipticalCoordinate(DateTime.Now);
+            Ecliptic mequator = Mars.EclipticalCoordinate(DateTime.Now);
+            Ecliptic jequator = Jupiter.EclipticalCoordinate(DateTime.Now);
+            Ecliptic sequator = Saturn.EclipticalCoordinate(DateTime.Now);
+            Ecliptic urquator = Uranus.EclipticalCoordinate(DateTime.Now);
+            Ecliptic nequator = Neptune.EclipticalCoordinate(DateTime.Now);
+            Ecliptic ecliptic = CoordinateSystem.Equatorial2Ecliptic(equator);
+          
+            Debug.WriteLine("SUN" + ecliptic.Longitude);
+            Debug.WriteLine("Moon" + mooquator.Longitude);
+            Debug.WriteLine("MERC: " + merquator.Longitude);
+            Debug.WriteLine("VENUS" + vequator.Longitude);
+            Debug.WriteLine("Mars" + mequator.Longitude);
+            Debug.WriteLine("Jup: " + jequator.Longitude);
+            Debug.WriteLine("Saturn: " + sequator.Longitude);
+            Debug.WriteLine("Urnuas: " + urquator.Longitude);
+            Debug.WriteLine("Neptune: " + nequator.Longitude);
+
             return View();
         }
-
+        
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
